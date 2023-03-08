f60a7fae58804a057c56f7e9d65f93e0f2f4e2da
Sarvar170803 <sarvarazamatov07062001@gmail.com>
2023-03-08 17:01:33 +0500

    abdulatif 20

--- src/pages/db_18-20/Page_20.jsx
+++ src/pages/db_18-20/Page_20.jsx
@@ -8,77 +8,85 @@ const Page_20 = () => {
         <h4 className='text-center bg-info py-2 mb-5'>СЕРТИФИКАЦИЯ ИКТ (ПО)</h4>
         <form className="row">
           <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Организация предоставленная услуга "СЕРТИФИКАЦИЯ ПО"</label>
+            <label className="form-label">Организация где проводится "Проверка и изучения" </label>
             <select name="" id="" className="form-select">
               <option>Прямая и обратная ссылка на БД "Организация"</option>
             </select>
           </div>
+        
           <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Основание для осуществления Сертификации</label>
+            <label className="form-label"> Основание для осуществления Проверки и изучении </label>
             <select name="" id="" className="form-select">
               <option selected>Прямая ссылка на СБ "Основание для оказания услуги ГУП "ЦКБ"</option>
             </select>
           </div>
+          
           <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Номер подписанного договора, запроса и т.д.  на проведение Сертификации</label>
-            <input type="text" id="" className="form-control" />
+            <label className="form-label"> Проведение Проверок и изучений совместно с </label>
+            <select name="" id="" className="form-select">
+              <option selected> Прямая ссылка на СБ "Совместные проверки и изучения"ЦКБ"</option>
+            </select>
           </div>
+
           <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Дата подписанного договора на проведение Сертификации (поступивщего запроса)</label>
+            <label className="form-label">Проверки и изучения осуществлена подразделением ГУП "ЦКБ" </label>
+            <select name="" id="" className="form-select">
+              <option selected>Прямая и обратная ссылка на БД "Подразделение ГУП "Центр кибербезопасности"</option>
+            </select>
+          </div>
+
+          <div className="mb-3 col-6 col-lg-4">
+            <label className="form-label">Сотрудники Центра участвовавшие в Проверки и изучении</label>
+            <select name="" id="" className="form-select">
+              <option selected>Прямая и обратная ссылка на БД "Подразделение ГУП "Центр кибербезопасности"</option>
+            </select>
+          </div>
+
+          <div className="mb-3 col-6 col-lg-4">
+            <label className="form-label">  Дата начала Проверки и изучения</label>
             <input type="date" name="" id="" className='form-control' />
           </div>
+
+
           <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Сумма оплаты за услугу по договору</label>
-            <select name="" id="" className="form-select">
-              <option selected>Прямая  ссылки на СБ "Сумма"</option>
-            </select>
-          </div>
-          <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Предмет Сертификации (точное наименование ПО)</label>
-            <select name="" id="" className="form-select">
-              <option selected>Прямая ссылка на СБ "Предметы Сертификации ПО"</option>
-            </select>
-          </div>
-          <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Услуга "СЕРТИФИКАЦИЯ ПО" осуществлена подразделением ГУП "ЦКБ"</label>
-            <select name="" id="" className="form-select">
-              <option selected>Прямая и обратная ссылка на БД "Подразделение ГУП "Центр кибербезопасности"</option>
-            </select>
-          </div>
-          <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Сотрудники Центра участвовавшие в Сертификации данного ПО</label>
-            <select name="" id="" className="form-select">
-              <option selected>Прямая и обратная ссылка на БД "Подразделение ГУП "Центр кибербезопасности"</option>
-            </select>
-          </div>
-          <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Дата начала предоставления услуги "Сертификации ПО"</label>
-            <input type="date" id="" className="form-select" />
-          </div>
-          <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Стадия реализации услуги (на какой стадии находится осуществление Сертификации ПО) </label>
+            <label className="form-label">Стадия реализации (на какой стадии находится осуществление Проверки и изучения)</label>
             <input type="text" id="" className="form-control" />
           </div>
+        
           <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Дата окончания предоставления услуги "Сертификации ПО"</label>
+            <label className="form-label">СРезультаты проведения Проверки и изучения </label>
+            <select name="" id="" className="form-select">
+              <option selected>  Прямая ссылка на СБ "Проверки и изучения"</option>
+            </select>
+          </div>
+
+          <div className="mb-3 col-6 col-lg-4">
+            <label className="form-label">Дата окончания предоставления Проверки и изучения  </label>
             <input type="date" id="" className="form-select" />
           </div>
+
           <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Дополнительная информация</label>
+            <label className="form-label">Дополнительная информация </label>
             <input type="text" id="" className="form-control" />
           </div>
+
+
           <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Итоговый документ по результатам реализации услуги "Экспертиза ИС на соответствие ИБ" (ДОК файл на внешней папке)</label>
-            <select name="" id="" className="form-select ">
-              <option>Прямая ссылка на ДОК файл с данными по конкретной организации на внешнем источнике (папке)</option>
+            <label className="form-label">Итоговый документ по результатам реализации Проверки и изучения (ДОК файл на внешней папке)</label>
+            <select name="" id="" className="form-select">
+              <option selected>Прямая ссылка на ДОК файл с данными по конкретной организации на внешнем источнике (папке)</option>
             </select>
           </div>
+
+
           <div className="mb-3 col-6 col-lg-4">
-            <label className="form-label">Источник информации об "СT"</label>
-            <select name="" id="" className="form-select ">
-              <option>Прямая и обратная ссылка на БД "ИСТОЧНИК ИНФОРМАЦИИ"</option>
+            <label className="form-label">Источник информации об "ПИ"</label>
+            <select name="" id="" className="form-select">
+              <option selected>Прямая и обратная ссылка на БД "ИСТОЧНИК ИНФОРМАЦИИ"</option>
             </select>
-          </div> 
+          </div>
+
+          
           <div className="mb-3 col-6 col-lg-4">
             <label className="form-label">Дата ввода первичной информации</label>
             <input type="date" name="" id="" className='form-control' />
